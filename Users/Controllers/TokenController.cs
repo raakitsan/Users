5fc44cf792ea18a70e24166387ba4be626397d45
raakitsan <brian.raak@gmail.com>
2022-08-31 18:57:07 -0500

    Can get Token now

---
+++ Users/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldService.Controllers
+{
+  public class TokenRequest
+  {
+    public string Email { get; set; }
+    public string Password { get; set; }
+  }
+
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TokenController : ControllerBase
+  {
+    // This should require SSL
+    [HttpPost]
+    public dynamic Post([FromBody] TokenRequest tokenRequest)
+    {
+      var token = TokenHelper.GetToken(tokenRequest.Email, tokenRequest.Password);
+      return new { Token = token };
+    }
+
+    // This should require SSL
+    [HttpGet]
+    public dynamic Get(string email, string password)
+    {
+      var token = TokenHelper.GetToken(email, password);
+      return new { Token = token };
+    }
+  }
+}
